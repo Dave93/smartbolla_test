141d3c3a351fa2d5cd0f55dd1121843f4a6a7067
shahaa <projects.salim95@gmail.com>
2021-06-04 09:15:15 +0500

    asd

--- components/Project/Project.desktop.js
+++ components/Project/Project.desktop.js
@@ -6,7 +6,9 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faYoutube } from "@fortawesome/free-brands-svg-icons";
 import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
-import SecureLogo from '../../public/secure.svg'
+import SecureLogo from "../../public/secure.svg";
+import GroupLogo from "../../public/Group.svg"
+import Rectangle from "../../public/Rectangle79.svg";
 
 library.add(faYoutube, faArrowLeft);
 
@@ -51,38 +53,61 @@ function Project({ project, onShowYoutube }) {
         key={project.ID}
         className="grid grid-cols-2 gap-4 items-center overflow-hidden mb-10"
       >
-        <div className="text-black">
-          <div className="border">
-            <div className="text-5xl p-10">Paycent</div>
-            <div className="text-2xl p-2">
+        <div className={styles.leftBlock}>
+          <div className={styles.border}>
+            <div className={styles.title}>Paycent</div>
+            <div className={`${styles.description} p-3`}>
               A payment system that will combine all the best features payment
               systems on the market.
             </div>
           </div>
-          <div className="mt-1 border flex">
-            <SecureLogo />
-            <div className="p-3">
+          <div className={`${styles.border} mt-1 flex`}>
+            <div className="p-3 align-items-center flex">
+              <SecureLogo />
               Secure transactions that do not require confidants
             </div>
           </div>
-          <div className="mt-1 border flex">
-            <SecureLogo />
-            <div className="p-3">Fast speed and low transaction fees;</div>
+          <div className={`${styles.border} mt-1 flex`}>
+            <div className="p-3 align-items-center flex">
+              <SecureLogo />
+              Secure transactions that do not require confidants
+            </div>
           </div>
-          <div className="mt-1 border flex">
-            <SecureLogo />
-            <div className="p-3">Reliability</div>
+          <div className={`${styles.border} mt-1 flex`}>
+            <div className="p-3 align-items-center flex">
+              <SecureLogo />
+              Secure transactions that do not require confidants
+            </div>
           </div>
-          <div className="mt-1 border flex">
-            <SecureLogo />
-            <div className="p-3">Without an account in the banking system</div>
+          <div className={`${styles.border} mt-1 flex`}>
+            <div className="p-3 align-items-center flex">
+              <SecureLogo />
+              Secure transactions that do not require confidants
+            </div>
           </div>
         </div>
-        <div className="...">
-          <motion.div initial="hidden" animate={controls} variants={logoBlock}>
-            <div className="items-center h-full justify-around relative ">
-              <img src={project.DETAIL_PICTURE} className="w-50" />
-              {project.PROPERTY_YOUTUBE_LINK_VALUE && (
+        <div className={styles.rightBlock}>
+          <div className={styles.logoBox}>
+            <GroupLogo className={styles.logoRight} />
+            {project.PROPERTY_YOUTUBE_LINK_VALUE && (
+              <div
+                className="absolute"
+                onClick={() => {
+                  onShowYoutube(project.PROPERTY_YOUTUBE_LINK_VALUE);
+                }}
+              >
+                <FontAwesomeIcon
+                  icon={faYoutube}
+                  size="lg"
+                  className="cursor-pointer text-red-500 w-12"
+                />
+              </div>
+            )}
+          </div>
+          {/* <motion.div initial="hidden" animate={controls} variants={logoBlock}> */}
+          {/* <div className="items-center h-full justify-around relative "> */}
+          {/* <img src={project.DETAIL_PICTURE} className="w-50" /> */}
+          {/* {project.PROPERTY_YOUTUBE_LINK_VALUE && (
                 <div
                   className="absolute"
                   onClick={() => {
@@ -95,10 +120,11 @@ function Project({ project, onShowYoutube }) {
                     className="cursor-pointer text-red-500 w-12"
                   />
                 </div>
-              )}
-            </div>
-          </motion.div>
+              )} */}
+          {/* </div> */}
+          {/* </motion.div> */}
         </div>
+        <Rectangle className={styles.rectangle}/>
       </div>
     </div>
   );
