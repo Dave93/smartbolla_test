386f1c80a7d8c530e48ba3672d6d8d2db2018787
Davron <yul.davron.93@gmail.com>
2021-07-05 21:22:56 +0500

    auth page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 const colors = require("tailwindcss/colors");
 module.exports = {
-  mode: "jit",
+  // mode: "jit",
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   variants: {
@@ -9,9 +9,11 @@ module.exports = {
   },
   plugins: [],
   theme: {
-    fontFamily: {
-      sans: ["Montserrat", ...defaultTheme.fontFamily.sans],
+    extend: {
+      fontFamily: {
+        sans: ["Montserrat", ...defaultTheme.fontFamily.sans],
+      },
     },
   },
-  important: true,
+  // important: true,
 };
